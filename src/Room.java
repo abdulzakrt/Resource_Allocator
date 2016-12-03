688feedd3e3afe118554a51ae943eb6b256fba0b
BesherHomsi <besherhomsi9@gmail.com>
2016-12-03 17:02:14 +0300

    setters and getters of this class
    RoomName
    RoomCounter (static)
    The number of seats in the room
    room size
    and if there is a projector or not in this room

--- src/Room.java
+++ src/Room.java
@@ -1,8 +1,48 @@
 
 public class Room extends Resource {
-
+        private String roomName;
+        private static int roomCounter;
+        private int numberOfSeats;
+        private char size;     //small s , mid. m, large l
+        private boolean proj_availability;
+        
+        
 	public Room() {
-		// TODO Auto-generated constructor stub
+		roomCounter++;
 	}
+        
+        public void setRoomName(String roomName){
+            this.roomName = roomName;
+        }
+        
+        public void setNumberOfSeats(int numberOfSeats){
+            this.numberOfSeats = numberOfSeats; 
+        }
+        
+        public void setRoomSize(char roomSize ){
+            this.size = roomSize;
+        }
+        
+        public void setPojectorAvil(boolean isAvailable){
+            this.proj_availability = isAvailable;
+        }
+        public String getRoomName(){
+            return this.roomName;
+        }        
+        
+        public int getNumberOfSeats(){
+            return this.numberOfSeats;
+        }
+        
+        public char getSizeOfRoom(){
+            return this.size;
+        }
 
+        public int get(){
+            return this.numberOfSeats;
+        }   
+        
+        public boolean isProjectAvailable(){
+            return this.proj_availability;
+        }
 }
